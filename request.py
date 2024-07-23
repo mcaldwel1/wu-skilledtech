8f2a1e864c793ea224b992966db221fafaf9bf91
mcaldwel1 <149115334+mcaldwel1@users.noreply.github.com>
2024-07-23 02:21:34 +0000

    innit 7

--- request.py
+++ request.py
@@ -1,23 +1,63 @@
 import requests 
+import json
 
-"""CLIENT_ID = '647a25ef09b3e9a6f00aa290'
+courseArray = [
+    "powerofai", 
+    "doverdata", 
+    "appliediotmay2024", 
+    "iotfundamentalsshell", 
+    "iotfundamentalsmay2024", 
+    "mpm365sb", 
+    "ai4-product-managers", 
+    "iotfapril2024", 
+    "cfda1", 
+    "appliediotsolutions", 
+    "iotsolutions", 
+    "iotfundamentals", 
+    "gradallmsexcel2", 
+    "advanced-cybersecurity-and-future-trends", 
+    "cybersecurity-fundamentals", 
+    "computer-programming-fundamentals", 
+    "jsmo", 
+    "mactdpwcpb", 
+    "mactdpwcp", 
+    "mac365", 
+    "excelbasic", 
+    "upltd", 
+    "timken-steel-data-analytics-ii", 
+    "ican", 
+    "test4", 
+    "gwmsoffice", 
+    "psgoodwill", 
+    "psmt", 
+    "tsda", 
+    "msoffice", 
+    "stw-job-skills-c"
+]
 
-grant_type = 'client_credentials'
-body_params = {'grant_type' : grant_type}
+course_Length = len(courseArray)
+c = 12
 
-url = 'https://www.wu-skilledtech.com/admin/api/v2/assessments/65cd2be39bc1aed0e6001d2b/responses'
-response = requests.post(url, data=body_params, auth = (CLIENT_ID)) 
+def get_new_url(): 
+    if(c < course_Length):
+        return courseArray[c] + "/contents"
+    else: 
+        return courseArray[0] + "/contents"
+    
+fetchUrl = "https://www.wu-skilledtech.com/admin/api/v2/courses/" + get_new_url()
 
-token_raw = json.loads(response.text)
-token = token_raw["AunFTUso15kMMpqHTyfm8CdZ6HRRjvrhkwtoUCWz"]
-
-headers = {"Authorization": "Bearer {}".format(token)}
-r = requests.get(url=url, headers=headers)
-print(r.text)"""
-
-r = requests.get(url="https://www.wu-skilledtech.com/admin/api/v2/assessments/65cd2be39bc1aed0e6001d2b/responses", 
+response = requests.get(url=fetchUrl, 
     headers={"Authorization": "Bearer AunFTUso15kMMpqHTyfm8CdZ6HRRjvrhkwtoUCWz", 
              "Lw-Client": "647a25ef09b3e9a6f00aa290",
              "Accept": "application/json"})
 
-print(r.text)
+parsed = json.loads(response.text)
+
+def get_id():
+    ("Pre-Course" in parsed)
+
+result = filter(get_id(), parsed)
+filtered = list(result)
+
+print(parsed['sections'][0]['title'])
+print(filtered)
